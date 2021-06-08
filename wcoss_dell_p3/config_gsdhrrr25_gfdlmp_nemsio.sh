b0597628945e154dac6e373bb2b17f798c05402a
Chan-Hoo.Jeon-NOAA <60152248+chan-hoo@users.noreply.github.com>
2021-06-07 20:34:29 -0400

    Update config_gsdhrrr25_gfdlmp_nemsio.sh

--- wcoss_dell_p3/config_gsdhrrr25_gfdlmp_nemsio.sh
+++ wcoss_dell_p3/config_gsdhrrr25_gfdlmp_nemsio.sh
@@ -24,8 +24,8 @@ CCPP_PHYS_SUITE="FV3_GFS_2017_gfdlmp"
 FCST_LEN_HRS="12"
 LBC_SPEC_INTVL_HRS="6"
 
-DATE_FIRST_CYCL="20190804"
-DATE_LAST_CYCL="20190804"
+DATE_FIRST_CYCL="20190801"
+DATE_LAST_CYCL="20190801"
 CYCL_HRS=( "12" )
 
 EXTRN_MDL_NAME_ICS="FV3GFS"
@@ -42,12 +42,13 @@ RUN_TASK_RUN_NEXUS="TRUE"
 FV3_EXEC_FN="NEMS.x"
 FV3_NML_BASE_SUITE_FN="input.nml.fv3gfs_aqm_FV3_GFS_2017_gfdlmp"
 FV3_NML_YAML_CONFIG_FN="fv3gfs_aqm.input.yml"
-#ENABLE_AQ="FALSE"
+WFLOW_XML_FN="fv3gfs_aqm_wflow.xml"
 
 AQM_CONFIG_DIR="/gpfs/dell2/emc/modeling/noscrub/Jianping.Huang/fv3sar/aqm/epa/data"
 AQM_EMIS_DIR="/gpfs/dell2/emc/modeling/noscrub/Jianping.Huang/fv3sar/aqm/bio"
 AQM_FIRE_DIR="/gpfs/dell2/emc/retros/noscrub/Barry.Baker/emissions/GSCE/GBBEPx.in.C401/Reprocessed"
-AQM_FIRE_FILE="GBBEPx_C401GRID.emissions_v003_20190804.nc"
+AQM_FIRE_FILE="GBBEPx_C401GRID.emissions_v003"
 AQM_LBCS_DIR="/gpfs/dell2/emc/retros/noscrub/Barry.Baker/emission"
 AQM_LBCS_FILES="gfs_bndy_chem_<MM>.tile7.000.nc"
 NEXUS_INPUT_DIR="/gpfs/dell2/emc/retros/noscrub/Barry.Baker/emissions"
+
